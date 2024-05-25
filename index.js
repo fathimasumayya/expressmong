cdcfd9e175195b5bd0baab6bfb6a9acd50304a51
fathimasumayya <sumiarafath893@gmail.com>
2024-05-25 20:28:09 +0530

    commit7

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-mongoose.Connection();
 
-mongoose.connect("mongodb://localhost:27017/avodhaEjs");
+mongoose.connect("mongodb://localhost:27017/myproject");
 
-const userSchema = mongoose.Schema({
+app.use(express.json());
+
+const studentSchema = new mongoose.Schema({
+  roll_no: Number,
   name: String,
 });
 
-const User = mongoose.model("User", userSchema);
+const Student = mongoose.model("Student", studentSchema);
+
+app.post("/api/create/", async (req, res) => {
+  const newStudent = new Student({
+    roll_no: req.body.roll_no,
+    name: req.body.name,
+  });
+  // newStudent
+  //   .save()
+  //   .then((result) => {
+  //     console.log(result);
+  //     res.json(result);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.json({ errorMsg: err });
+  //   });
+
+  const stu = await newStudent.save();
+  res.json(stu);
+});
 
 app.listen(3000, () => console.log("Server is running"));
